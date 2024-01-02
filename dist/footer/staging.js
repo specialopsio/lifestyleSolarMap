f3414d2bfbbc480c3cad6cd7c69cdc547a9c7367
Specialist R <r@rug.tech>
2024-01-02 17:27:37 +0000

    Corrected url

--- dist/footer/staging.js
+++ dist/footer/staging.js
@@ -1,4 +1,4 @@
-if (window.location.href.indexOf("undefined") !== -1) {
+if (window.location.href.indexOf("map.lifestylesolar.com") !== -1) {
   const sidebarItems = [{
       label: "Installed",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--ic" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
