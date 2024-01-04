487afe58d9b1f1bfd2a0b9a333b86b7a37bb4e31
tom-larkin <111458319+mr0chill@users.noreply.github.com>
2024-01-03 17:22:23 -0700

    Gulp Commit: Updating ./dist/*

--- dist/footer/staging.js
+++ dist/footer/staging.js
@@ -49,16 +49,6 @@ if (window.location.href.indexOf("map.lifestylesolar.com") !== -1) {
       id: "sidebarCancelled",
       selected: false,
       functions: [""]
-    },
-    {
-      label: "Test 123",
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--ic" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10s10-4.47 10-10S17.53 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m3.59-13L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41z"></path>
-               </svg>`,
-      count: 431,
-      id: "sidebarTest",
-      selected: false,
-      functions: [""]
     }
 
   ];
